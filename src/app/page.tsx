7815c2be4c036c9eeaaa62a6716e6e36fd1b2d14
tefloon <tefloon@gmail.com>
2023-10-02 16:00:45 +0200

    Cleaned up page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 import Intro from "@/components/intro";
 import Image from "next/image";
 
-let upperMargin = 24;
-
 export default function Home() {
   return (
     <>
-      {/* <div className={`pt-${upperMargin}`}></div> */}
       <main>
         <Intro />
       </main>
